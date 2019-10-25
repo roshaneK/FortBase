0177c3a342c56f3a31a97ba564b8950385b1221f
dprageethroshane <dprageethroshane@gmail.com>
2019-10-25 16:13:55 -0700

    Configued nlayered Web API project with EF

---
+++ NLayer_EF_SQLServer/FortBase/FortBase.API/Controllers/UserController.cs
@@ -0,0 +1,27 @@
+﻿using FortBase.Data;
+using FortBase.Model;
+using FortBase.Service;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FortBase.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly FortBaseDbContext dbContext;
+
+        public UserController(FortBaseDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        [HttpPost("SaveUser")]
+        public IActionResult SaveUser(UserModel userModel)
+        {
+            var userService = new UserService(dbContext);
+            var result = userService.SaveUser(userModel);
+            return Ok(result);
+        }
+    }
+}
